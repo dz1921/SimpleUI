405196275613543fbabc3f64d2ff3748f6dcc83d
dz1921 <147485631+dz1921@users.noreply.github.com>
2023-10-30 18:35:24 +0000

    Add files via upload

---
+++ SimpleUI/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.GraphicsConfiguration;
+public class Main {
+    public static void main(String[] args) {
+        SimpleUI.main(args);
+    }
+
+}
